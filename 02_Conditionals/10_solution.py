abacd34263c4e0f81298d07cc66c533d85eeb157
Abhinav <abhinavkumarjha02@gmail.com>
2024-08-19 17:25:26 +0530

    Question 10 solved

--- 02_Conditionals/10_solution.py
+++ 02_Conditionals/10_solution.py
@@ -1,7 +1,12 @@
-pet species = "Dog"
+pet = input("Enter either cat or dog :")
 age = int(input("Enter the age of the pet species :"))
-if age <2:
-    print("Puppy food")
-elif age >=2:
-    print("Adult food")
-elif age 
+if pet == "Dog":
+    if age <2:
+        print("Puppy food")
+    else:
+        print("Adult food")
+if pet == "Cat":
+    if age <5:
+        print("Junior cat food")
+    else:
+        print("senior cat food")
